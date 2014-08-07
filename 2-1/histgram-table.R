9cca031b20e16826d3f9cdd152a8a3aa3659f340
Nobuyuki Furuyama <nobuyuki@frym.jp>
2014-08-08 00:45:25 +0900

    Draw Counts graph.

--- 2-1/histgram-table.R
+++ 2-1/histgram-table.R
@@ -1,5 +1,6 @@
 # setwd("/Users/furuyama/github/manga_sience_of_statistics/2-1/")
 # objects()
+# source("histgram-table.R")
 rm (umai.ramen, output.table, umai.ramen.histhistgram)
 umai.ramen <- read.csv("ramen.csv", header=T, stringsAsFactors=F)
 umai.ramen.histhistgram <- hist(umai.ramen[,2],breaks = c(500,600,700,800,900,1000),plot=F,right=FALSE)
@@ -11,3 +12,26 @@ output.table[2:6,3] <- umai.ramen.histhistgram$counts
 output.table[2:6,4] <- umai.ramen.histhistgram$density * 100
 # output.table <- rbind(output.table, c("計",NA,sum(umai.ramen.histhistgram$counts),sprintf("%.2f",sum(umai.ramen.histhistgram$density * 100))))
 output.table[7,] <- c("計",NA,sum(umai.ramen.histhistgram$counts),sprintf("%.2f",sum(umai.ramen.histhistgram$density * 100)))
+# 出力
+output.table
+
+# data.frame作成
+df <- data.frame(mids = umai.ramen.histhistgram$mid, counts = umai.ramen.histhistgram$counts)
+
+# グラフ書く
+library(ggplot2)
+g <- ggplot(
+  df,
+  aes (
+    x = mids,
+    y = counts,
+    fill = counts
+  )
+)
+g <- g + geom_bar(
+  stat = "identity",
+)
+g <- g + xlab("mids")
+g <- g + ylab("Counts")
+g <- g + ggtitle("manga_sience_of_statistics 2-1")
+plot(g)
